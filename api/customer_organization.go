c34b1ed35857e56ab83167d9bef41d541ab7cf9b
Jakob Steiner <kosmoz@users.noreply.github.com>
2026-02-11 11:59:11 +0100

    fix: rename customer feature "notifications" to "alerts" and validate "alerts" depends on "deployment_targets" (#1809)
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>
    Co-authored-by: Philip Miglinci <pmig@glasskube.com>

--- api/customer_organization.go
+++ api/customer_organization.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"fmt"
+	"slices"
 	"time"
 
 	"github.com/distr-sh/distr/internal/types"
+	"github.com/distr-sh/distr/internal/validation"
 	"github.com/google/uuid"
 )
 
@@ -11,6 +14,16 @@ type CreateUpdateCustomerOrganizationRequest struct {
 	Name     string                              `json:"name"`
 	ImageID  *uuid.UUID                          `json:"imageId,omitempty"`
 	Features []types.CustomerOrganizationFeature `json:"features,omitempty"`
+}
+
+func (r *CreateUpdateCustomerOrganizationRequest) Validate() error {
+	if slices.Contains(r.Features, types.CustomerOrganizationFeatureAlerts) &&
+		!slices.Contains(r.Features, types.CustomerOrganizationFeatureDeploymentTargets) {
+		return validation.NewValidationFailedError(fmt.Sprintf("feature %v requires feature %v",
+			types.CustomerOrganizationFeatureAlerts, types.CustomerOrganizationFeatureDeploymentTargets))
+	}
+
+	return nil
 }
 
 type CustomerOrganization struct {
