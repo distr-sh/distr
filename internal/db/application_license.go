4d67f8c9f7c8d935f74c4f31dbabdab2762597c8
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-02-16 11:30:15 +0100

    fix(backend): add missing parameter in application license query
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- internal/db/application_license.go
+++ internal/db/application_license.go
@@ -22,7 +22,7 @@ const (
 	applicationLicenseWithVersionsOutputExpr = applicationLicenseOutputExpr + `,
 		coalesce((
 		   	SELECT array_agg(
-				row(av.id, av.created_at, av.archived_at, av.name, av.application_id,
+				row(av.id, av.created_at, av.archived_at, av.name, av.link_template, av.application_id,
 					av.chart_type, av.chart_name, av.chart_url, av.chart_version)
 				ORDER BY av.created_at ASC
 			)
