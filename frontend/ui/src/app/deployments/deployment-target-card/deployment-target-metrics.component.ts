d5969a2c72b92e8b39db8bb9ccf7857d4b235b61
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-03-24 12:57:15 +0100

    chore: metrics status dot

--- frontend/ui/src/app/deployments/deployment-target-card/deployment-target-metrics.component.ts
+++ frontend/ui/src/app/deployments/deployment-target-card/deployment-target-metrics.component.ts
@@ -1,18 +1,19 @@
-import {OverlayModule} from '@angular/cdk/overlay';
-import {NgStyle, PercentPipe} from '@angular/common';
-import {Component, computed, input, signal} from '@angular/core';
-import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {faExclamation, faHardDrive} from '@fortawesome/free-solid-svg-icons';
-import {BytesPipe} from '../../../util/units';
-import {drawerFlyInOut} from '../../animations/drawer';
-import {dropdownAnimation} from '../../animations/dropdown';
-import {modalFlyInOut} from '../../animations/modal';
-import {DeploymentTargetLatestMetrics} from '../../services/deployment-target-metrics.service';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { NgStyle, PercentPipe } from '@angular/common';
+import { Component, computed, input, signal } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faExclamation, faHardDrive } from '@fortawesome/free-solid-svg-icons';
+import { BytesPipe } from '../../../util/units';
+import { drawerFlyInOut } from '../../animations/drawer';
+import { dropdownAnimation } from '../../animations/dropdown';
+import { modalFlyInOut } from '../../animations/modal';
+import { DeploymentTargetLatestMetrics } from '../../services/deployment-target-metrics.service';
+import { StatusDotDirective } from "../../components/status-dot";
 
 @Component({
   selector: 'app-deployment-target-metrics',
   templateUrl: './deployment-target-metrics.component.html',
-  imports: [OverlayModule, BytesPipe, PercentPipe, NgStyle, FaIconComponent],
+  imports: [OverlayModule, BytesPipe, PercentPipe, NgStyle, FaIconComponent, StatusDotDirective],
   animations: [modalFlyInOut, drawerFlyInOut, dropdownAnimation],
   styleUrls: ['./deployment-target-metrics.component.scss'],
 })
