a02ca8e27e877b172b1f66a5ce5e85dd25aabc2a
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-03-24 12:20:29 +0100

    chore: disk warning icon

--- frontend/ui/src/app/deployments/deployment-target-card/deployment-target-metrics.component.ts
+++ frontend/ui/src/app/deployments/deployment-target-card/deployment-target-metrics.component.ts
@@ -1,22 +1,28 @@
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PercentPipe, NgStyle } from '@angular/common';
-import { Component, input, signal } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { BytesPipe } from '../../../util/units';
 import { drawerFlyInOut } from '../../animations/drawer';
 import { dropdownAnimation } from '../../animations/dropdown';
 import { modalFlyInOut } from '../../animations/modal';
 import { DeploymentTargetLatestMetrics } from '../../services/deployment-target-metrics.service';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faExclamation, faHardDrive } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-deployment-target-metrics',
   templateUrl: './deployment-target-metrics.component.html',
-  imports: [OverlayModule, BytesPipe, PercentPipe, NgStyle],
+  imports: [OverlayModule, BytesPipe, PercentPipe, NgStyle, FaIconComponent],
   animations: [modalFlyInOut, drawerFlyInOut, dropdownAnimation],
   styleUrls: ['./deployment-target-metrics.component.scss'],
 })
 export class DeploymentTargetMetricsComponent {
   public readonly metrics = input.required<DeploymentTargetLatestMetrics>();
   protected readonly hovered = signal(false);
+  protected readonly anyDiskWarning = computed(() => this.metrics().diskMetrics?.some(disk => disk.bytesUsed / disk.bytesTotal > 0.75))
+
+  protected readonly faHardDrive = faHardDrive
+  protected readonly faExclamation = faExclamation
 
   protected getUsageDegrees(value: number | undefined): string {
     return (360 * (value ?? 0)).toFixed() + 'deg';
