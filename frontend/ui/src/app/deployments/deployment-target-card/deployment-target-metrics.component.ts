44e90f1bce56182479797e75326de6efbe93c3a4
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-03-24 12:12:58 +0100

    chore: optimize gauge css

--- frontend/ui/src/app/deployments/deployment-target-card/deployment-target-metrics.component.ts
+++ frontend/ui/src/app/deployments/deployment-target-card/deployment-target-metrics.component.ts
@@ -1,16 +1,16 @@
-import {OverlayModule} from '@angular/cdk/overlay';
-import {PercentPipe} from '@angular/common';
-import {Component, input, signal} from '@angular/core';
-import {BytesPipe} from '../../../util/units';
-import {drawerFlyInOut} from '../../animations/drawer';
-import {dropdownAnimation} from '../../animations/dropdown';
-import {modalFlyInOut} from '../../animations/modal';
-import {DeploymentTargetLatestMetrics} from '../../services/deployment-target-metrics.service';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PercentPipe, NgStyle } from '@angular/common';
+import { Component, input, signal } from '@angular/core';
+import { BytesPipe } from '../../../util/units';
+import { drawerFlyInOut } from '../../animations/drawer';
+import { dropdownAnimation } from '../../animations/dropdown';
+import { modalFlyInOut } from '../../animations/modal';
+import { DeploymentTargetLatestMetrics } from '../../services/deployment-target-metrics.service';
 
 @Component({
   selector: 'app-deployment-target-metrics',
   templateUrl: './deployment-target-metrics.component.html',
-  imports: [OverlayModule, BytesPipe, PercentPipe],
+  imports: [OverlayModule, BytesPipe, PercentPipe, NgStyle],
   animations: [modalFlyInOut, drawerFlyInOut, dropdownAnimation],
   styleUrls: ['./deployment-target-metrics.component.scss'],
 })
@@ -18,9 +18,7 @@ export class DeploymentTargetMetricsComponent {
   public readonly metrics = input.required<DeploymentTargetLatestMetrics>();
   protected readonly hovered = signal(false);
 
-  protected getPercentClass(usage: number | undefined): string {
-    const val = Math.ceil((usage || 0) * 100);
-    const mod5 = val % 5;
-    return (mod5 === 0 ? val : val - mod5 + 5).toFixed();
+  protected getUsageDegrees(value: number | undefined): string {
+    return (360 * (value ?? 0)).toFixed() + 'deg';
   }
 }
