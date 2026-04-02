c78c302d00c78df6d852cde285d589ea64e45885
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-04-02 10:28:16 +0200

    chore: add loading indicator

--- frontend/ui/src/app/components/timeseries-table.component.ts
+++ frontend/ui/src/app/components/timeseries-table.component.ts
@@ -1,5 +1,5 @@
 import {AsyncPipe, DatePipe} from '@angular/common';
-import {Component, inject, input, signal} from '@angular/core';
+import {Component, computed, inject, input, signal} from '@angular/core';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faThumbtack, faThumbtackSlash} from '@fortawesome/free-solid-svg-icons';
@@ -22,6 +22,43 @@ export interface TimeseriesSource {
   loadAfter(after: Date): Observable<TimeseriesEntry[]>;
 }
 
+export class TimeseriesSourceWithStatus implements TimeseriesSource {
+  public readonly batchSize: number;
+  private readonly loadingRW = signal(false);
+  public readonly loading = this.loadingRW.asReadonly();
+
+  constructor(private readonly source: TimeseriesSource) {
+    this.batchSize = source.batchSize;
+  }
+
+  load(): Observable<TimeseriesEntry[]> {
+    this.loadingRW.set(true);
+    return this.source.load().pipe(
+      tap({
+        finalize: () => this.loadingRW.set(false),
+      })
+    );
+  }
+
+  loadBefore(before: Date): Observable<TimeseriesEntry[]> {
+    this.loadingRW.set(true);
+    return this.source.loadBefore(before).pipe(
+      tap({
+        finalize: () => this.loadingRW.set(false),
+      })
+    );
+  }
+
+  loadAfter(after: Date): Observable<TimeseriesEntry[]> {
+    this.loadingRW.set(true);
+    return this.source.loadAfter(after).pipe(
+      tap({
+        finalize: () => this.loadingRW.set(false),
+      })
+    );
+  }
+}
+
 export interface TimeseriesExporter {
   getFileName(): string;
   export(): Observable<Blob>;
@@ -31,7 +68,27 @@ export interface TimeseriesExporter {
   selector: 'app-timeseries-table',
   template: `
     @if (entries$ | async; as entries) {
-      <div class="flex flex-col gap-4" [class.flex-col-reverse]="!newestFirst()">
+      <div class="relative flex flex-col gap-4" [class.flex-col-reverse]="!newestFirst()">
+        @if (sourceWithStatus().loading()) {
+          <div class="absolute top-0 inset-e-0 rounded-xl bg-white dark:bg-gray-900 z-10 p-2">
+            <output class="flex justify-center items-center gap-2 text-gray-700 dark:text-gray-400 text-xs">
+              <svg
+                aria-hidden="true"
+                class="size-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor" />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill" />
+              </svg>
+              <span>Loading&hellip;</span>
+            </output>
+          </div>
+        }
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead
             class="dark:border-gray-600 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-800 dark:text-gray-400 sr-only">
@@ -131,8 +188,10 @@ export class TimeseriesTableComponent {
   protected isExporting = false;
   protected readonly pinnedEntryId = signal<string | null>(null);
 
+  protected readonly sourceWithStatus = computed(() => new TimeseriesSourceWithStatus(this.source()));
+
   private readonly accumulatedEntries$: Observable<TimeseriesEntry[]> = combineLatest([
-    toObservable(this.source),
+    toObservable(this.sourceWithStatus),
     toObservable(this.live),
   ]).pipe(
     switchMap(([source, live]) => {
