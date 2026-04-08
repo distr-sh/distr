2650a469d12b35deff4194904fed79c70b458fd1
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-04-08 12:23:24 +0200

    chore: update rate limit policy

--- internal/handlers/agent.go
+++ internal/handlers/agent.go
@@ -610,7 +610,7 @@ var (
 		// Adding 25% margin and assuming that people have at most 10 deployments on a single agent we arrive at
 		// (12+10*12)*1.25 = 11*12*1.25 = 11*15
 		// also adding 2 for the metric reports
-		(11*15)+2,
+		500,
 		1*time.Minute,
 		httprate.WithKeyFuncs(middleware.RateLimitCurrentDeploymentTargetIdKeyFunc),
 	)
