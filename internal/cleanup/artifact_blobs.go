50ba1730666623aea177c5fbcc80858b0ab09bee
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-04-17 11:47:50 +0200

    chore: delete blobs more eagerly

--- internal/cleanup/artifact_blobs.go
+++ internal/cleanup/artifact_blobs.go
@@ -34,6 +34,24 @@ func RunArtifactBlobCleanup(ctx context.Context) error {
 
 	var skippedStillReferenced int
 	var skippedMinAge int
+	var deleted int
+	var errs []error
+
+	flushDelete := func(batch []s3types.ObjectIdentifier) error {
+		result, err := s3Client.DeleteObjects(ctx, &s3.DeleteObjectsInput{
+			Bucket: &bucket,
+			Delete: &s3types.Delete{Objects: batch, Quiet: new(true)},
+		})
+		if err != nil {
+			return fmt.Errorf("deleting S3 objects: %w", err)
+		}
+		deleted += len(batch) - len(result.Errors)
+		for _, e := range result.Errors {
+			err := fmt.Errorf("failed to delete %s: %s", util.PtrDerefOrDefault(e.Key), util.PtrDerefOrDefault(e.Message))
+			errs = append(errs, err)
+		}
+		return nil
+	}
 
 	var toDelete []s3types.ObjectIdentifier
 	paginator := s3.NewListObjectsV2Paginator(s3Client, &s3.ListObjectsV2Input{Bucket: &bucket})
@@ -49,33 +67,27 @@ func RunArtifactBlobCleanup(ctx context.Context) error {
 			if strings.HasPrefix(*obj.Key, "chunks/") {
 				continue
 			}
-			if obj.LastModified != nil && obj.LastModified.After(cutoff) {
-				skippedMinAge++
-				continue
-			}
 			if _, ok := referencedDigests[*obj.Key]; ok {
 				skippedStillReferenced++
 				continue
 			}
+			if obj.LastModified != nil && obj.LastModified.After(cutoff) {
+				skippedMinAge++
+				continue
+			}
 			toDelete = append(toDelete, s3types.ObjectIdentifier{Key: obj.Key})
+			if len(toDelete) >= 1000 {
+				if err := flushDelete(toDelete); err != nil {
+					return err
+				}
+				toDelete = toDelete[:0]
+			}
 		}
 	}
 
-	var deleted int
-	var errs []error
-	for batch := range slices.Chunk(toDelete, 1000) {
-		result, err := s3Client.DeleteObjects(ctx, &s3.DeleteObjectsInput{
-			Bucket: &bucket,
-			Delete: &s3types.Delete{Objects: batch, Quiet: new(true)},
-		})
-		if err != nil {
-			return fmt.Errorf("deleting S3 objects: %w", err)
-		}
-
-		deleted += len(batch) - len(result.Errors)
-		for _, e := range result.Errors {
-			err := fmt.Errorf("failed to delete %s: %s", util.PtrDerefOrDefault(e.Key), util.PtrDerefOrDefault(e.Message))
-			errs = append(errs, err)
+	if len(toDelete) > 0 {
+		if err := flushDelete(toDelete); err != nil {
+			return err
 		}
 	}
 
