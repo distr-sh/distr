36550c6bbdb532171d7f58bd0327c203eefa911d
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-04-30 12:18:13 +0200

    chore: drop index
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- internal/migrations/sql/96_deployment_target_metrics_index.down.sql
+++ internal/migrations/sql/96_deployment_target_metrics_index.down.sql
@@ -1,4 +1,6 @@
 DROP INDEX IF EXISTS DeploymentTargetMetrics_deployment_target_id_created_at_id;
 
+CREATE INDEX IF NOT EXISTS DeploymentTargetMetrics_deployment_target_id ON DeploymentTargetMetrics (deployment_target_id);
+
 -- Restore the misnamed index on DeploymentRevisionStatus (originally added by a typo in migration 37)
 CREATE INDEX IF NOT EXISTS DeploymentTargetMetrics_created_at ON DeploymentRevisionStatus (created_at DESC);
