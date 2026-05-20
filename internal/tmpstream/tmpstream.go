98b78b34139e275310f8d6a3a366fde5994757a0
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-05-20 15:53:12 +0200

    fix(registry): add support for chunk uploads larger than 5GB
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- internal/tmpstream/tmpstream.go
+++ internal/tmpstream/tmpstream.go
@@ -6,9 +6,19 @@ import (
 	"github.com/distr-sh/distr/internal/env"
 )
 
+type ReadSeekAt interface {
+	io.ReadSeeker
+	io.ReaderAt
+}
+
+type ReadSeekAtCloser interface {
+	ReadSeekAt
+	io.Closer
+}
+
 // TmpStream represents a resource that can be accessed via io interfaces and destroyed if no longer needed.
 type TmpStream interface {
-	Get() (io.ReadSeekCloser, error)
+	Get() (ReadSeekAtCloser, error)
 	Destroy() error
 }
 
